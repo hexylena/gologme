5f8371c049b155d6f8dc2ca32aa7978b9806727f
Eric Rasche <rasche.eric@gmail.com>
2016-01-10 01:45:35 -0600

    Store data in a sqlite3 database

--- server/main.go
+++ server/main.go
@@ -5,40 +5,79 @@ package main
 import (
 	"fmt"
 	"github.com/erasche/gologme"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net"
 	"net/http"
 	"net/rpc"
 )
 
-type Golog int
-
-func (t *Golog) Log(args gologme.RpcArgs, result *gologme.Result) error {
-	return Log(args, result)
+type Golog struct {
+	Db *sql.DB
 }
 
-func Log(args gologme.RpcArgs, result *gologme.Result) error {
+func (t *Golog) Log(args gologme.RpcArgs, result *gologme.Result) error {
+	tx, err := t.Db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err := tx.Prepare("insert into windowLogs (time, name) values (?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
 	log.Printf("%d logs from [%d]\n", args.Length, args.User)
 	for i, w := range args.Windows {
-		log.Printf("\t%d %s\n", i, w)
+		_, err = stmt.Exec(w.Time.Unix(), w.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		if i >= args.Length - 1{
 			break
 		}
 	}
+
+	tx.Commit()
 	*result = 0
 	return nil
 }
 
 func main() {
+	db, err := sql.Open("sqlite3", "file.db")
+	if err != nil{
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	dbCreation := `
+	create table if not exists windowLogs (
+		id integer not null primary key,
+		time integer,
+		name text
+	)
+	`
+
+	_, err = db.Exec(dbCreation)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	golog := new(Golog)
+	golog.Db = db
+
 	rpc.Register(golog)
 	rpc.HandleHTTP()
-	l, e := net.Listen("tcp", ":8080")
-	if e != nil {
-		log.Fatal("listen error:", e)
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal("listen error:", err)
 	}
 	fmt.Println("Listening...")
-	err := http.Serve(l, nil)
+
+	err = http.Serve(l, nil)
 	if err != nil {
 		log.Fatal("Error serving:", err)
 	}
